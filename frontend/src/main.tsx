06bf07c6334589053493aa132cc0f208940fcf94
Vince Magwili <vsmagwili@up.edu.ph>
2026-06-28 20:30:23 +0800

    feat: added vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { inject } from "@vercel/analytics";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
 import App from "./App.tsx";
@@ -19,6 +20,8 @@ const queryClient = new QueryClient({
         },
     },
 });
+
+inject();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
