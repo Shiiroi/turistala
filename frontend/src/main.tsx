2495f364405af6a51245ec3a39b398715d24a6b7
Vince Magwili <vsmagwili@up.edu.ph>
2026-06-28 23:38:43 +0800

    fix: vercel analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
 import App from "./App.tsx";
@@ -21,7 +21,6 @@ const queryClient = new QueryClient({
     },
 });
 
-inject();
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
@@ -29,6 +28,7 @@ createRoot(document.getElementById("root")!).render(
             <AuthProvider>
                 <ToastProvider>
                     <App />
+                    <Analytics />
                     <ToastViewport />
                 </ToastProvider>
             </AuthProvider>
